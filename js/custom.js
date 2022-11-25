1182ba3ac18642d4211bf5cbc482494db7a1c391
Mario Rufo Ariza <mariojra6@gmail.com>
2022-11-25 12:23:37 +0100

    Archivos actualizados

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,11 @@
 window.addEventListener("load",function() {
     document.getElementById('loader').classList.toggle('loader2');
 })
+
+function cambiar(){
+    document.getElementById('cambiomapa').src="../img/grande.png";
+}
+  
+function volver(){
+    document.getElementById('cambiomapa').src="../img/mapafuera.png";
+}
